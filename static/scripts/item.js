593afda070f57f96ad0d4e806c0cc16352e5c027
tanelm <tanelm@tlu.ee>
2019-09-12 21:57:50 +0300

    Lesson 2: createElement

---
+++ static/scripts/item.js
@@ -0,0 +1,39 @@
+const x = window.location;
+console.log(x);
+const urlParams = new URLSearchParams(window.location.search);
+const title = urlParams.get('title');
+const cost = urlParams.get('cost');
+const src = urlParams.get('src');
+console.log(title,cost,src);
+//alert('Title $(title) cost: $(cost) path: $(src)');
+
+const container = document.createElement("div");
+container.className = "itemContainer";
+
+const image = document.createElement("img");
+image.src = src;
+image.className = "item__image";
+
+const titleElement = document.createElement("h2");
+titleElement.textContent = title;
+titleElement.className = "item__title"
+
+const description = "Üks väga väga lahe auto mis saab sõita 200-ga ja kuuenda käiguga kummi vilistada";
+
+const textElement = document.createElement("p")
+textElement.textContent = description;
+textElement.className = "item__description";
+
+const costElement = document.createElement("div");
+costElement.textContent = cost;
+costElement.className = "item__price";
+
+container.append(titleElement);
+container.append(image);
+container.append(textElement);
+container.append(costElement);
+
+window.addEventListener("load", () =>{
+    const app = document.getElementById("item-body");
+    app.append(container);
+});
