863a68ec7e1d4b9a2249737f3b950a1c4267ba17
tanelm <tanelm@tlu.ee>
2019-09-21 15:50:39 +0300

    Lesson3: Using babel with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,15 @@ module.exports = {
             }
         ])
     ],
+    module: {
+        rules: [
+          {
+            test: /\.js/,
+            exclude: /node_modules/,
+            use:  "babel-loader",
+          }
+        ]
+      },
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
