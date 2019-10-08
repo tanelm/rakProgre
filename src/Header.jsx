c0bfa13eaee701c822705f21e7c1ed3eeb48c557
tanelm <tanelm@tlu.ee>
2019-10-08 18:59:32 +0300

    Lesson 5: header design

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
-
+import {userIcon, cart} from "./icons.js";
+import "./header.css";
 
 const Header = () => {
     return(
@@ -9,10 +10,17 @@ const Header = () => {
             <img className="header__logo" src="/img/tlu_logo.jpg" />
             </Link>
             <div className="header__buttons">
-                <button>Login/Signup</button>
-                <button>Cart</button>
+                <div className={"header__button"}>
+                    <img src={userIcon} />
+                    <div className={"header__button-text"}>Login/<br/>Register</div>
+                </div>
+                <div className={"header__button"}>
+                    <img src={cart} style={{height: 35}} />
+                    <div className={"header__button-text"}>Cart<br/></div>
+                </div>                    
             </div>
         </div>
+
     );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
     module: {
         rules: [
           {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
             enforce: 'pre',
             test: /\.(js|jsx)$/,
             exclude: /node_modules/,
