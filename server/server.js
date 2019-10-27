f25fa518a230b7bc3c46dd3c714c3d12bb007398
tanelm <tanelm@tlu.ee>
2019-10-27 12:03:28 +0200

    asd

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(itemRouter);
 app.use(userRouter);
 
 
-app.get("/*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
 });
 
